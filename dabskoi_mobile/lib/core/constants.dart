43467be8f15cd4ca7629575b50b3d6c21bdb4f22
lanstheprodigy <lanstheprodigy@icloud.com>
2025-08-25 22:05:36 +0700

    refactor ip

--- dabskoi_mobile/lib/core/constants.dart
+++ dabskoi_mobile/lib/core/constants.dart
@@ -1,4 +1,6 @@
+// TODO: change to your ip address
+const String ip = '192.168.1.55';
 const String apiBaseUrl = String.fromEnvironment(
   'API_URL',
-  defaultValue: 'http://192.168.1.7:2000',
+  defaultValue: 'http://$ip:3001',
 );
